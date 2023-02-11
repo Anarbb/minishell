0a09ff736690f6c98f8087a03b348f3bd1cf3ed6
Lina Sabik <lina12345.2002@gmail.com>
2023-02-11 18:30:10 +0100

    echo update

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -3,34 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aarbaoui <aarbaoui@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: lsabik <lsabik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 16:13:14 by aarbaoui          #+#    #+#             */
-/*   Updated: 2023/02/11 16:33:11 by aarbaoui         ###   ########.fr       */
+/*   Updated: 2023/02/11 18:29:38 by lsabik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    ft_echo(char **args)
+int	echo_cmd(char **cmd)
 {
-    int i;
-    int n;
+	int		i;
+	int		j;
+	int	new_line;
 
-    i = 1;
-    n = 0;
-    while (args[i])
-    {
-        if (ft_strcmp(args[i], "-n") == 0)
-        {
-            n = 1;
-            i++;
-        }
-        ft_putstr_fd(args[i], 1);
-        if (args[i + 1])
-            ft_putstr_fd(" ", 1);
-        i++;
-    }
-    if (!n)
-        ft_putstr_fd("\n", 1);
+	i = 1;
+	new_line = 1;
+	while(cmd[i] &&cmd[i][0] == '-' && cmd[i][1] == 'n')
+	{
+		j = 1;
+		while(cmd[i][j] == 'n')
+			j++;
+		if (cmd[i][j] == '\0')
+			new_line = 0;
+		else
+			break;
+		i++;
+	}
+	while (cmd[i])
+	{
+		ft_putstr_fd(cmd[i++], 1);
+		if (cmd[i])
+			write(1, " ", 1);
+	}
+	if (new_line == 1)
+		write(1, "\n", 1);
+	return (EXIT_SUCCESS);
 }
