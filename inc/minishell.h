b54222c814e23a3a0ef5e88a19e70769c1a2f7de
Anas <anas@arbaoui.me>
2023-02-12 14:00:33 +0100

    linux doesnt have libc

--- inc/minishell.h
+++ inc/minishell.h
@@ -13,10 +13,22 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <libc.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft.h"
 
 # define RESET			"\033[0m"
 # define RED			"\033[1;31m"
