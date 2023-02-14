2bd36d82d139746d6dd0e5d50def9736097d0acf
Anas <anas@arbaoui.me>
2023-02-14 15:17:56 +0100

    fixed error: variable ‘len’ set but not used in add_env

--- src/utils/add_env.c
+++ src/utils/add_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   add_env.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsabik <lsabik@student.42.fr>              +#+  +:+       +#+        */
+/*   By: aarbaoui <aarbaoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 11:26:01 by lsabik            #+#    #+#             */
-/*   Updated: 2023/02/13 14:05:04 by lsabik           ###   ########.fr       */
+/*   Updated: 2023/02/14 15:17:37 by aarbaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,10 @@
 void    add_env(t_shell *shell, char *name, char *value)
 {
 	int i;
-	int len;
 	char *tmp;
 
 	i = 0;
 	(void)value;
-	len = ft_strlen(name);
 	while (shell->env[i])
 	{
 		if (ft_strcmp(shell->env[i], name) == 0)
