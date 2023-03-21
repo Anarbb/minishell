22a0df64b20237a523b703b3ba6e9bae5be5d4bf
Anas <anas@arbaoui.me>
2023-03-21 15:16:43 +0000

    fixed leaks in 0+0

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: aarbaoui <aarbaoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 10:18:36 by lsabik            #+#    #+#             */
-/*   Updated: 2023/03/20 21:37:13 by aarbaoui         ###   ########.fr       */
+/*   Updated: 2023/03/21 15:16:20 by aarbaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,11 @@ char	*after_dollar(t_shell *shell, char *str, char *tmp, int i)
 			free(value);
 		}
 		else
-			tmp = ft_join(tmp, ft_substr(str, i, 1));
+		{
+			tmp2 = ft_substr(str, i, 1);
+			tmp = ft_join(tmp, tmp2);
+			free(tmp2);
+		}
 		i++;
 	}
 	return (tmp);
